06e76fc28f53606cd43a4dc1fbb3dbc134872905
Craig Wilkinson <craig@dangerfarms.com>
2019-09-28 19:06:53 +0100

    Add volume slider and mute checkbox to master node

--- src/components/nodes/MasterNode/index.js
+++ src/components/nodes/MasterNode/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tone from 'tone';
 import DraggableBox from '../../common/DraggableBox';
 import OutputInput from '../../common/OutputInput';
 import { useNode } from '../../../hooks/node';
+import Checkbox from '../../common/Checkbox';
+import Slider from '../../common/Slider/input';
 
 const MasterNode = ({inputs, onClickInput}) => {
+  const [volume, setVolume] = useState(0);
+  const [isMuted, setIsMuted] = useState(false);
   const nodeRef = useNode(Tone.Master, inputs);
+
+  useEffect(() => {
+    nodeRef.current.volume.value = volume;
+    nodeRef.current.mute = isMuted;
+  }, [volume, isMuted, nodeRef]);
 
   return (
     <DraggableBox title="Master">
-      <p>Icon would go here.</p>
-      <p>So would volume slider.</p>
+      <Checkbox checked={isMuted} onChange={() => setIsMuted(!isMuted)} label="Muted" />
+      <Slider min={-30} max={0} onChange={e => setVolume(e.target.value)} label="Volume" />
       <OutputInput onClick={onClickInput} isInput />
     </DraggableBox>
   )
